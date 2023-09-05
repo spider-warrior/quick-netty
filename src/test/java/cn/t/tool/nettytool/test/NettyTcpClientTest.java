46fc6e9dbc344a77b350ecd76b58bf5553bbb74b
36296 <362961910@qq.com>
2023-09-05 10:13:40 +0800

    优化 NettyTcpServer.java

--- src/test/java/cn/t/tool/nettytool/test/NettyTcpClientTest.java
+++ src/test/java/cn/t/tool/nettytool/test/NettyTcpClientTest.java
@@ -1,10 +1,13 @@
 package cn.t.tool.nettytool.test;
 
 import cn.t.tool.nettytool.daemon.DaemonConfig;
+import cn.t.tool.nettytool.daemon.DaemonService;
 import cn.t.tool.nettytool.daemon.client.NettyTcpClient;
+import cn.t.tool.nettytool.daemon.listener.DaemonListener;
 import cn.t.tool.nettytool.initializer.DaemonConfigBuilder;
 import cn.t.tool.nettytool.initializer.NettyTcpChannelInitializer;
 import cn.t.tool.nettytool.test.handler.TcpClientHandler;
+import io.netty.channel.Channel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.logging.LogLevel;
@@ -25,13 +28,30 @@ public class NettyTcpClientTest {
         //logging
         daemonConfigBuilder.configLogLevel(LogLevel.DEBUG);
         //idle
-        daemonConfigBuilder.configIdleHandler(0, 0, 180);
         //fetch message handler
         daemonConfigBuilder.configHandler(Collections.singletonList(ch -> new TcpClientHandler()));
         DaemonConfig<SocketChannel> daemonConfig = daemonConfigBuilder.build();
         NettyTcpChannelInitializer channelInitializer = new NettyTcpChannelInitializer(daemonConfig);
-        NettyTcpClient nettyTcpClient = new NettyTcpClient("test-client", "www.shansong.com", 80, channelInitializer, new NioEventLoopGroup(1), false, true);
+        NettyTcpClient nettyTcpClient = new NettyTcpClient("test-client", "123", 80, channelInitializer, new NioEventLoopGroup(1), true, true);
+        nettyTcpClient.addListenerList(Collections.singletonList(new DaemonListenerDemo()));
         nettyTcpClient.start();
         Thread.sleep(10000);
+    }
+
+    private static class DaemonListenerDemo implements DaemonListener {
+        @Override
+        public void startup(DaemonService server, Channel channel) {
+            System.out.println("DaemonListenerDemo#startup(DaemonService server, Channel channel)");
+        }
+
+        @Override
+        public void close(DaemonService server, Channel channel) {
+            System.out.println("DaemonListenerDemo#close(DaemonService server, Channel channel)");
+        }
+
+        @Override
+        public void close(DaemonService server, Channel channel, Throwable t) {
+            System.out.println("DaemonListenerDemo#close(DaemonService server, Channel channel, Throwable t): " + Thread.currentThread());
+        }
     }
 }
